acc848e09d2663f3c6d37a963e1bf1566e3ad3dd
David Batranu <dbatranu@gmail.com>
2013-03-11 18:09:52 +0200

    Carousel on homepage

--- src/theme/plone/ro/browser/interfaces.py
+++ src/theme/plone/ro/browser/interfaces.py
@@ -1,6 +1,11 @@
 from zope.interface import Interface
+from zope.viewlet.interfaces import IViewletManager
 
 
 class IThemeSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer.
+    """
+
+class IHomepageCarousel(IViewletManager):
+    """Image Carousel on the homepage
     """
